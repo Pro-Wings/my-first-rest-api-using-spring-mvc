10b1a6f400e755b8cca14eba589076ca5c67e53b
JD <jayprowings@gmail.com>
2023-08-24 12:06:00 +0530

    global exception handling in Spring

--- src/main/java/com/prowings/controller/HelloWorldController.java
+++ src/main/java/com/prowings/controller/HelloWorldController.java
@@ -1,17 +1,41 @@
 package com.prowings.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.prowings.entity.Person;
+import com.prowings.service.VoterService;
+
+
+@RestController
 public class HelloWorldController {
+	
+	@Autowired
+	VoterService voterService;
 	
 	@GetMapping("/hello")
 	public @ResponseBody String hello()
 	{
 		return "Hello... welcome to Spring-MVC-Rest!!!";
 	}
+
+	@PostMapping("/vote")
+	public ResponseEntity<String> doVote(@RequestBody Person person)
+	{
+		String res = voterService.doVote(person);
+		
+		ResponseEntity<String> response = new ResponseEntity<String>(res, HttpStatus.OK);
+		
+		return response;
+	}
+	
+	
 
 }
---
+++ src/main/java/com/prowings/entity/Error.java
@@ -0,0 +1,34 @@
+package com.prowings.entity;
+
+import org.springframework.http.HttpStatus;
+
+public class Error {
+	
+	private HttpStatus httpStatus;
+	
+	private String errorMessage;
+
+	public Error() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public HttpStatus getHttpStatus() {
+		return httpStatus;
+	}
+
+	public void setHttpStatus(HttpStatus httpStatus) {
+		this.httpStatus = httpStatus;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+	
+	
+}
